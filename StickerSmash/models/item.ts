d91e6f01d8f9592c64b447bc14ebafe1214d0b74
Aarav Desai <aaravdesai@berkeley.edu>
2024-12-02 23:30:00 -0800

    take away the watchers pls

--- StickerSmash/models/item.ts
+++
@@ -1,9 +0,0 @@
-// This is a TypeScript interface.
-// Learn More: https://www.typescriptlang.org/docs/handbook/interfaces.html
-export interface ItemModel {
-  id?: string;
-  itemDescription: string;
-  itemImage: string;
-  itemName: string;
-  itemPrice: number;
-}
